37792be79f5cefeca5a79374b00bec53676bd79a
Christopher Coballes <cobecoballes@gmail.com>
2024-05-03 16:01:26 +0800

    Create esp32_header.h

---
+++ esp32_header.h
@@ -0,0 +1,35 @@
+#ifndef ESP32_HEADER_H
+#define ESP32_HEADER_H
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "audio_element.h"
+#include "audio_pipeline.h"
+#include "audio_event_iface.h"
+#include "audio_common.h"
+#include "i2s_stream.h"
+#include "esp_peripherals.h"
+#include "periph_wifi.h"
+#include "input_key_service.h"
+#include "driver/gpio.h"
+#include "lvgl/lvgl.h"
+#include "esp_freertos_hooks.h"
+#include "downmix.h"
+#include "audio_mem.h"
+#include "raw_stream.h"
+#include "filter_resample.h"
+#include "esp_sip.h"
+#include "g711.h"
+#include "mp3_decoder.h"
+#include "drv/stmpe610.h"
+#include "drv/ili9341.h"
+#include "drv/disp_spi.h"
+
+#endif // ESP32_HEADER_H
